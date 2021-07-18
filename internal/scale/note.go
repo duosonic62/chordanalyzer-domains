34c0b0a4e3f47b09cf055e4fbbb3d24e168daf73
Sekiguchi Riku <l.life.style.of.sustainability@gmail.com>
2021-07-18 21:22:04 +0900

    add get interval note

--- internal/scale/note.go
+++ internal/scale/note.go
@@ -1,5 +1,7 @@
 package scale
 
+import "errors"
+
 type Note struct {
 	name          string
 	intervalFromC int
@@ -79,4 +81,50 @@ func (n Note) CalculateInterval(compareNNote Note) (*Interval, error) {
 	}
 
 	return intervalFromNumber(compareNNote.intervalFromC + (scaleNoteCount - n.intervalFromC))
+}
+
+//GetIntervalNote はInterval分離れたノートを取得する
+func (n Note) GetIntervalNote(interval *Interval) (*Note, error) {
+	noteInterval := n.intervalFromC + interval.value
+	if noteInterval >= scaleNoteCount {
+		noteInterval = noteInterval - scaleNoteCount
+	}
+
+	note, err := noteFromInterval(noteInterval)
+	if err != nil {
+		return nil, err
+	}
+
+	return note, nil
+}
+
+func noteFromInterval(interval int) (*Note, error) {
+	switch interval {
+	case 0:
+		return &Notes.C, nil
+	case 1:
+		return &Notes.CS, nil
+	case 2:
+		return &Notes.D, nil
+	case 3:
+		return &Notes.DS, nil
+	case 4:
+		return &Notes.E, nil
+	case 5:
+		return &Notes.F, nil
+	case 6:
+		return &Notes.FS, nil
+	case 7:
+		return &Notes.G, nil
+	case 8:
+		return &Notes.GS, nil
+	case 9:
+		return &Notes.A, nil
+	case 10:
+		return &Notes.AS, nil
+	case 11:
+		return &Notes.B, nil
+	default:
+		return nil, errors.New("note interval must be 0 - 11")
+	}
 }
