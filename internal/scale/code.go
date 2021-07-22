72d2a6f82c8b37380a7cc014920f85bb29eb2a71
Sekiguchi Riku <l.life.style.of.sustainability@gmail.com>
2021-07-22 21:14:29 +0900

    add new code comments

--- internal/scale/code.go
+++ internal/scale/code.go
@@ -1,8 +1,26 @@
 package scale
 
+import "errors"
+
 type Code struct {
 	Name  string
 	Root  *Note
 	Notes *[]Note
 }
 
+func NewCode(notes []Note) (*Code, error) {
+	if len(notes) < 3 {
+		return nil, errors.New("need at least 3 chord tones")
+	}
+
+	// ルートノート
+	root := notes[0]
+
+	// トライアドを検出する
+	traid := notes[0:2]
+
+	// トライアド以外を検出する
+	tention := notes[3:]
+
+	return nil, nil
+}
