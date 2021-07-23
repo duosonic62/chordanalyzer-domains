8bb07e7d5fc71300bf1759f774f115ab8af9a587
Sekiguchi Riku <l.life.style.of.sustainability@gmail.com>
2021-07-23 19:09:59 +0900

    update code structure

--- internal/scale/code.go
+++ internal/scale/code.go
@@ -2,13 +2,26 @@ package scale
 
 import "errors"
 
-type Code struct {
-	Name  string
-	Root  *Note
-	Notes *[]Note
+type Code interface {
+	Name() string
+	Root() *Note
+	Notes() *[]Note
 }
 
-func NewCode(notes []Note) (*Code, error) {
+type Triad struct {
+	name  string
+	root  *Note
+	notes *[]Note
+}
+
+type TensionCode struct {
+	name  string
+	root  *Note
+	triadNotes *[]Note
+	tensionNotes *[]Note
+}
+
+func NewCode(notes []Note) (*TensionCode, error) {
 	if len(notes) < 3 {
 		return nil, errors.New("need at least 3 chord tones")
 	}
@@ -17,10 +30,50 @@ func NewCode(notes []Note) (*Code, error) {
 	root := notes[0]
 
 	// トライアドを検出する
-	traid := notes[0:2]
+	triad := notes[0:2]
+	isTriad(triad)
 
 	// トライアド以外を検出する
 	tention := notes[3:]
 
-	return nil, nil
+	return &TensionCode{
+		Name:  "",
+		Root:  &root,
+		Notes: &notes,
+	}, nil
+}
+
+func isTriad(notes []Note) bool {
+	// トライアドは3和音
+	if len(notes) != 3 {
+		return false
+	}
+
+	root := notes[0]
+
+	// major 3 or minor 3?
+	third, err := root.CalculateInterval(notes[1])
+	fifth, err := root.CalculateInterval(notes[2])
+	if err != nil {
+		return false
+	}
+
+	// major triad
+	if third.IsEquivalent(&Intervals.Major3) && fifth.IsEquals(&Intervals.Perfect5) {
+		return true
+	}
+	// minor triad
+	if !(third.IsEquivalent(&Intervals.Minor3) || fifth.IsEquals(&Intervals.Perfect5)) {
+		return true
+	}
+	// augmented triad
+	if !(third.IsEquivalent(&Intervals.Major3) || fifth.IsEquals(&Intervals.Sharp5)) {
+		return true
+	}
+	// diminished triad
+	if !(third.IsEquivalent(&Intervals.Minor3) || fifth.IsEquals(&Intervals.Sharp4)) {
+		return true
+	}
+
+	return false
 }
