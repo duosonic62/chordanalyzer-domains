9e6aaa2f8a5f8fd8e3bfbc8862dbbd189e516d4f
Sekiguchi Riku <l.life.style.of.sustainability@gmail.com>
2021-08-03 20:35:38 +0900

    add tention code

--- internal/scale/tention_test.go
+++ internal/scale/tention_test.go
@@ -14,3 +14,24 @@ func TestNewTensionCode(t *testing.T) {
 		t.Error("Expected: err, but error is Nil")
 	}
 }
+
+func TestTension_Name(t *testing.T) {
+	actual, _ := NewTensionCode([]Note{Notes.C, Notes.E, Notes.G, Notes.A})
+	if actual.Name() != "C6" {
+		t.Error("Expected: C6, but actual: " + actual.name)
+	}
+}
+
+func TestTension_Root(t *testing.T) {
+	actual, _ := NewTensionCode([]Note{Notes.C, Notes.E, Notes.G, Notes.A})
+	if !Notes.C.Equals(actual.Root()) {
+		t.Error("Expected: C, but actual: " + actual.Root().name)
+	}
+}
+
+func TestTension_Notes(t *testing.T) {
+	actual, _ := NewTensionCode([]Note{Notes.C, Notes.E, Notes.G, Notes.A})
+	if !actual.Notes()[0].Equals(&Notes.C) && actual.Notes()[1].Equals(&Notes.E) && actual.Notes()[2].Equals(&Notes.G) && actual.Notes()[3].Equals(&Notes.A) {
+		t.Error("Expected: C, E, G, A, but actual: " + actual.Notes()[0].name + ", " + actual.Notes()[1].name + ", " + actual.Notes()[2].name + ", " + actual.Notes()[3].name)
+	}
+}
