c4a9862d59df4f0af8a2be3a2f9e2e6d20764006
Sekiguchi Riku <l.life.style.of.sustainability@gmail.com>
2021-08-13 10:33:15 +0900

    fix main

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -1,50 +1,5 @@
 package main
 
-import (
-	"fmt"
-	"github.com/duosonic62/codanalyzer-domains/cmd/factory"
-	"github.com/duosonic62/codanalyzer-domains/internal/code"
-	"github.com/duosonic62/codanalyzer-domains/internal/tone"
-)
-
 func main() {
-	codes, err := factory.ReadCodesFromJson("codes.json")
-	if err != nil {
-		fmt.Println(err)
-	}
 
-	triads, err := factory.ReadCodesFromJson("triad.json")
-	if err != nil {
-		fmt.Println(err)
-	}
-
-	codeStructureBases, err := factory.MakeCodeStructureBase(codes)
-	if err != nil {
-		fmt.Println(err)
-	}
-	for _, code := range *codeStructureBases {
-		fmt.Println(code.Name())
-		fmt.Println(code.GetIntervals())
-	}
-
-	triadStructureBases, err := factory.MakeTriadStructureBase(triads)
-	if err != nil {
-		fmt.Println(err)
-	}
-	for _, code := range *triadStructureBases {
-		fmt.Println(code.Name())
-		fmt.Println(code.GetIntervals())
-	}
-
-	tones := [] tone.Interval{
-		tone.Intervals.R,
-		tone.Intervals.Major3,
-		tone.Intervals.Major7,
-		tone.Intervals.Natural9,
-		tone.Intervals.Sharp11,
-		tone.Intervals.Natural13,
-	}
-	seventhAddUpper, _ := code.NewCodeStructureBase("9", &tones)
-	fmt.Println(seventhAddUpper.Name())
-	fmt.Println(seventhAddUpper.GetIntervals())
 }
