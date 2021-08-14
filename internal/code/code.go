15d391488610d1fc76fa93b665d4b164f3ce46eb
Sekiguchi Riku <l.life.style.of.sustainability@gmail.com>
2021-08-14 14:03:23 +0900

    add code factory method implements

--- internal/code/code.go
+++ internal/code/code.go
@@ -1,6 +1,9 @@
 package code
 
-import "github.com/duosonic62/codanalyzer-domains/internal/scale"
+import (
+	"github.com/duosonic62/codanalyzer-domains/internal/scale"
+	"github.com/pkg/errors"
+)
 
 type Code interface {
 	Name() string
@@ -19,6 +22,68 @@ func NewCodeFactory(intervals []scale.Interval) *Factory {
 	}
 }
 
-func (f Factory) CreateCodes() (error, []Code) {
-	return nil, nil
+func (f Factory) Build() ([]Code, error) {
+
+	if len(f.intervals) == 3 {
+		triads, err := f.createTriads()
+		if err != nil {
+			return nil, errors.Wrap(err, "can't build codes, there are incorrect intervals")
+		}
+		return triads, nil
+	}
+
+	tensions, err := f.createTension()
+	if err != nil {
+		return nil, errors.Wrap(err, "can't build codes, there are incorrect intervals")
+	}
+	return tensions, nil
+}
+
+func (f Factory) createTriads() ([]Code, error) {
+	allNotes := scale.AllNotes()
+	triads := make([]Code, len(allNotes))
+	for i, root := range scale.AllNotes() {
+		notes, err := getNotes(root, f.intervals)
+		if err != nil {
+			return nil, err
+		}
+		triad, err := NewTriad(notes)
+		if err != nil {
+			return nil, err
+		}
+		triads[i] = triad
+	}
+
+	return triads, nil
+}
+
+func (f Factory) createTension() ([]Code, error) {
+	allNotes := scale.AllNotes()
+	tensions := make([]Code, len(allNotes))
+	for i, root := range scale.AllNotes() {
+		notes, err := getNotes(root, f.intervals)
+		if err != nil {
+			return nil, err
+		}
+		tension, err := NewTensionCode(notes)
+		if err != nil {
+			return nil, err
+		}
+		tensions[i] = tension
+	}
+
+	return tensions, nil
+}
+
+func getNotes(root scale.Note, intervals []scale.Interval) ([]scale.Note, error) {
+	notes := make([]scale.Note, len(intervals))
+	for i, interval := range intervals {
+		n, err := root.GetIntervalNote(&interval)
+		if err != nil {
+			return nil, err
+		}
+		notes[i] = *n
+	}
+
+	return notes, nil
 }
