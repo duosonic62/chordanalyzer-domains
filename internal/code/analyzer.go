8c4d35c3b738ca828b0a0c29a8a25c72748d704d
Sekiguchi Riku <l.life.style.of.sustainability@gmail.com>
2021-08-25 21:31:59 +0900

    add analyzer

--- internal/code/analyzer.go
+++ internal/code/analyzer.go
@@ -1,9 +1,19 @@
 package code
 
 type Analyzer interface {
-	AnalyzeIncludedCodes(code *Code) ([]Code, error)
+	AnalyzeIncludedCodes(target Code) []Code
 }
 
 type analyzer struct {
+	collection Collection
+}
 
+func NewAnalyzer(collection Collection) Analyzer {
+	return analyzer{collection: collection}
+}
+
+func (a analyzer) AnalyzeIncludedCodes(target Code) []Code {
+	return a.collection.Filter(func(code Code) bool {
+		return target.Contains(code)
+	})
 }
