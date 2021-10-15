fab17589319759b356a932b02c6813feddaacc26
Sekiguchi Riku <l.life.style.of.sustainability@gmail.com>
2021-10-15 20:52:26 +0900

    fix to show only c chord

--- cmd/cli/list.go
+++ cmd/cli/list.go
@@ -3,12 +3,13 @@ package cli
 import (
 	"fmt"
 	"github.com/duosonic62/codanalyzer-domains/internal/code"
+	"github.com/duosonic62/codanalyzer-domains/internal/scale"
 	"github.com/spf13/cobra"
 )
 
 var (
 	listCmd = &cobra.Command{
-		Use: "list all available codes",
+		Use: "List all available codes\n Show only chords whose root note is C",
 		Run: runList,
 	}
 )
@@ -18,7 +19,9 @@ func runList(cmd *cobra.Command, args []string) {
 }
 
 func listAction() {
-	Analyzer.AllCodes().ForEach(func(code code.Code) {
+	Analyzer.AllCodes().Filter(func(code code.Code) bool {
+		return code.Root().Equals(&scale.Notes.C)
+	}).ForEach(func(code code.Code) {
 		fmt.Println(code.Name())
 	})
 }
