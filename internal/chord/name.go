494a8af3dcc7ebb735d8c649fb524e2bd965e567
Sekiguchi Riku <l.life.style.of.sustainability@gmail.com>
2021-10-31 18:48:53 +0900

    add default implements

--- internal/chord/name.go
+++ internal/chord/name.go
@@ -4,10 +4,18 @@ import "github.com/duosonic62/chordanalyzer-domains/internal/scale"
 
 type Name interface {
 	Get() string
-	Equals() bool
+	Equals(name string) bool
 }
 
 type name struct {
 	root scale.Note
 	tension string
+}
+
+func (n name) Get() string {
+	return n.root.String() + n.tension
+}
+
+func (n name) Equals(name string) bool {
+	return name == n.Get()
 }
